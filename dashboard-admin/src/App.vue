4ce8d44851062d6b30ac2fd67fc40131a280b9f0
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-09-21 14:58:49 +0200

    configured pinia
    adding pinia to the project

--- dashboard-admin/src/App.vue
+++ dashboard-admin/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
     <div> Hello </div>
+    <Dashboard />
+    
   </div>
 </template>
 
 <script>
+import Dashboard from './components/Dashboard.vue'
+
 
 export default {
+  
   name: 'App',
+
   components: {
+    Dashboard,
   }
 }
 </script>
---
+++ dashboard-admin/src/components/Dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Dashboard</h1>
+  {{ users }}
+</template>
+
+<script>
+  import { useUsersStore } from '@/store/users';
+  import { mapStores, mapState} from 'pinia';
+
+export default {
+    name: 'UserDashboard',
+    computed: {
+      ...mapStores(useUsersStore),
+      ...mapState(useUsersStore, ['users'])
+    },
+    data() {
+        return {
+            message: ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

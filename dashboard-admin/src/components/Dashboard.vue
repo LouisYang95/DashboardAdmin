a0fc82e1bc2df406c0914cbdb7791d420e43fa4a
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-09-22 23:08:12 +0200

    we adapt the table with every value
    Adding some value and modal

--- dashboard-admin/src/components/Dashboard.vue
+++ dashboard-admin/src/components/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>DashBoard</h1>
   <br />
-  <button><router-link to="/create-products">CREATE</router-link></button>
+  <!-- board -->
   <table class="table table-striped">
     <thead>
       <tr>
@@ -9,6 +9,8 @@
         <th scope="col">Name</th>
         <th scope="col">Description</th>
         <th scope="col">Price</th>
+        <th scope="col">Stock</th>
+        <th scope="col">#</th>
       </tr>
     </thead>
     <tbody
@@ -25,22 +27,40 @@
         <td>
           {{ product.descriptions }}
         </td>
-        <td>{{ product.price }} euro - {{ product.stock }}</td>
-        <button @click="this.mainStore.selectProduct(product.id)">ME</button>
+        <td>{{ product.price }} euro</td>
+        <td> {{product.stock}} / {{product.stock}} </td>
+        <td>
+          <button
+            class="btn btn-outline-primary"
+            @click="this.modalStore.showModal();this.mainStore.selectProduct(product.id); "
+          >
+            Edit
+          </button>
+          <UseModal v-show="isModalVisible" @close="this.modalStore.showModal()" />
+        </td>
       </tr>
     </tbody>
   </table>
-  <button type="submit" @click="this.mainStore.editProduct(id)">Edit</button>
 </template>
 
 <script>
 // import our store
-import { useStore } from "@/store/users";
+import { useStore } from "@/store/useProduct";
 import { mapStores, mapState } from "pinia";
 import { useProduct } from "@/store/index";
+import UseModal from "./Modal.vue";
+import {useModal} from "@/store/useModal.js";
 
 export default {
   name: "UserDashboard",
+  components: { UseModal },
+  methods: {
+
+  },
+  beforeMount() {
+    // beforeMount function permit the call before the component is mounted
+    this.productsStore.getProducts(); // call the function getProducts in users.js store with the computed property store
+  },
   computed: {
     //create a computed property that returns the id given + store -> there productsStore
     ...mapStores(useStore),
@@ -50,18 +70,15 @@ export default {
     ...mapStores(useProduct),
     ...mapState(useProduct, ["changed"]),
     ...mapState(useProduct, ["id"]),
-  },
-  beforeMount() {
-    // beforeMount function permit the call before the component is mounted
-    this.productsStore.getProducts(); // call the function getProducts in users.js store with the computed property store
+    // create a computed property which returns the id given + store -> there modalStore
+    ...mapStores(useModal),
+    // create a computed named 'isModalVisible' containing the state : state.isModalVisible in users.js store
+    ...mapState(useModal, ["isModalVisible"]),
   },
 };
 </script>
 
-
 <style scoped>
-
-
 li {
   list-style: none;
   text-align: left;
@@ -73,7 +90,7 @@ ul {
   padding: 0;
 }
 
-.table.table-striped{
+.table.table-striped {
   width: 60%;
   margin: 0 auto;
   text-align: center;
